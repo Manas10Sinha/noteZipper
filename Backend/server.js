1d1b0d68b6151163393955762df348548c33c62a
sinhapradeep <sinhapradeep125@gmail.com>
2023-10-25 21:40:16 +0530

    second commit

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const noteRoutes = require("./routes/noteRoutes");
 const { notFound, errorHandler } = require("./Middleware/errorMiddleware");
+const path = require("path");
 
 const app = express();
 dotenv.config();
@@ -13,7 +14,20 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
+//------------------------------Deployment------------------------------------------
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "frontend/build")));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+//------------------------------Deployment------------------------------------------
 /*
 //API end point GET
 app.get("/", (req, res) => {
